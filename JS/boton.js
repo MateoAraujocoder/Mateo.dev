5ebdb64c2cadc29aa3f1986b732ba22a59d4f33b
Mateo Araujo <oliaramateo@gmail.com>
2024-01-14 09:45:17 -0300

    responsive

---
+++ JS/boton.js
@@ -0,0 +1,9 @@
+// JavaScript para manejar el clic en el botón de hamburguesa
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.nav').style.display = 'block';
+});
+
+// JavaScript para manejar el clic en el botón de cerrar
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.nav').style.display = 'none';
+});
